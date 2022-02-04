134b8806002b1a9b5d89d368d53f407e30c78986
brandonpani <bbrandonpaniagua@gmail.com>
2022-02-04 11:01:26 -0700

    cambio de idioma

--- js/scripts.js
+++ js/scripts.js
@@ -18,18 +18,43 @@ function loop() {
 if (textDispay!=null) {
 
     loop()
-}
+};
 
-const checkLang = document.getElementById('lang');
-checkLang.addEventListener('click', clickLang);
+// const checkLang = document.getElementById('lang');
+// checkLang.addEventListener('click', clickLang);
 
-function clickLang(e) {
-    if(this.checked){
-        window.location.href="/index.html";
-    }else {
-        window.location.href="/en/index.html";
-    }
-}
+// function clickLang(e) {
+//     if(this.checked){
+//         window.location.href="/index.html";
+//     }else {
+//         window.location.href="/en/index.html";
+//     }
+// };
+
+
+const select = document.querySelector('#select');
+const idiomas = document.querySelector('#idiomas');
+const contenidoSelect = document.querySelector('#contenidoSelect');
+
+document.querySelectorAll('#idiomas > .option').forEach((option) => {
+    option.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log('e.currentTarget.innerHTML');
+        contenidoSelect.innerHTML = e.currentTarget.innerHTML;
+        idiomas.classList.toggle('active');
+        select.classList.toggle('active');
+    })
+});
+
+select.addEventListener('click', () => {
+    select.classList.toggle('active');
+    idiomas.classList.toggle('active');
+});
+
+
+
+
+
 
 // function loop () {
 //     textDispay.innerHTML = currentFrase.join('')
