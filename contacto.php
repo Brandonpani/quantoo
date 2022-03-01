c047c484e86b3cf7fd1ac3c66c4e9418294e9c5d
brandonpani <bbrandonpaniagua@gmail.com>
2022-03-01 08:28:09 -0700

    cambio de numero paginas espanol

--- contacto.php
+++ contacto.php
@@ -185,7 +185,7 @@ if ( !session_id() ) {
                   <ul>
 
                       <li class="footer-link"><a href="mailto:contacto@quanto.mx">contacto@quanto.mx</a></li>
-                      <li class="footer-link"><a href="tel:+528141612470" class="tel">Llama a un asesor</a></li>
+                      <li class="footer-link"><a href="tel:+528185264543" class="tel">Llama a un asesor</a></li>
                   </ul>
               </div>
 
