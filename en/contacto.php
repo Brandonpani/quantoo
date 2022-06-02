89e4c13b7ef6ca4cd731ff7774d6f42791a738b0
Brandon <bbrandonpaniagua@gmail.com>
2022-06-02 12:52:35 -0600

    version estilos 1.4 english

--- en/contacto.php
+++ en/contacto.php
@@ -56,7 +56,7 @@ if ( !session_id() ) {
     />
 
     <!-- estilos css -->
-    <link rel="stylesheet" href="../css/estilos.css?v=1.3" />
+    <link rel="stylesheet" href="../css/estilos.css?v=1.4" />
   </head>
   <body>
 
