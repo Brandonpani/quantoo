3cb94b70af4383ef41c03527c02dd946a070b43d
Brandon <bbrandonpaniagua@gmail.com>
2022-06-03 09:24:16 -0600

    navegador delay scroll bueno

--- js/menu.js
+++ js/menu.js
@@ -13,55 +13,27 @@ document.getElementsByTagName("body")[0].addEventListener("click",function(evt){
 
 const body = document.body;
 
-
-
 let lastScroll = 0;
-
-
 
 window.addEventListener("scroll", () => {
 
   const currentScroll = window.pageYOffset;
-
-
-
   if (currentScroll <= 0) {
-
     body.classList.remove("scroll-up");
-
-
-
     return;
-
   }
-
-
 
   if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
-
     body.classList.remove("scroll-up");
-
     body.classList.add("scroll-down");
-
   } else if (
-
     currentScroll < lastScroll &&
-
     body.classList.contains("scroll-down")
-
   ) {
-
     body.classList.remove("scroll-down");
-
     body.classList.add("scroll-up");
-
   }
-
-
-
   if (Math.abs(currentScroll - lastScroll) > 150) {
     lastScroll = currentScroll;
-
   }
-
 });
