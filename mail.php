d71dbf03e6af229452f6eb5638cdf7070df1f3f8
BrandonPaniagua <bbrandonpaniagua@gmail.com>
2022-10-26 17:13:31 -0600

    falto declarar la variable ip

--- mail.php
+++ mail.php
@@ -1,5 +1,5 @@
 <?php
- ip = $_SERVER['REMOTE_ADDR'];
+ $ip = $_SERVER['REMOTE_ADDR'];
  $captcha = $_POST['g-recaptcha-response'];
  $secretkey = "6LerdSQfAAAAADJ1FbWZ0TjJVAUlsjWSXJwL5FhP"; 
  $respuesta = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secretkey&response=$captcha&remoteip=$ip");
